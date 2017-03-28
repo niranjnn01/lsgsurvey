5d2472ab19052ef1751c1c78a50aefeaf021d301
Rakesh <rakesh.various@gmail.com>
2017-03-28 06:57:18 +0530

    added map of ward to home page

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -6,6 +6,13 @@
 		<h4>കടൽത്തീരം വാർഡ്</h4>
 		<p>സീ വ്യൂ വാർഡിനെ സംപൂർണ പങ്കാളിത്ത ജനാധിപത്യ വാർഡാക്കി മാറ്റുന്നതിനുവേണ്ടിയുള്ള വിവരശേഖരണ യജ്ഞം</p>
 
+		<div class="row">
+			<div class="col-md-12">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15740.576515300027!2d76.3219367133614!3d9.496191178982988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0884563f7cc197%3A0xc0a00d66f6350eb!2sSea+View+Ward%2C+Alappuzha%2C+Kerala!5e0!3m2!1sen!2sin!4v1490664055422"
+								width="600" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
+		</div>
+
 	</div>
 	<div class="col-md-3">
 
@@ -59,6 +66,8 @@
 				<td>45</td>
 			</tr>
 		</table>
+
+
 
 	</div>
 </div>
